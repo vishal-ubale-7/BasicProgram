8b11a67cdfee3f70df359a15198a85390cab148e
Mr.Vishal Ubale <149001221+vishal-ubale-7@users.noreply.github.com>
2025-05-29 18:30:22 +0530

    Update ReverseString.java

--- src/main/basicprogram/ReverseString.java
+++ src/main/basicprogram/ReverseString.java
@@ -4,12 +4,12 @@ public class ReverseString
 {
     public static void main(String[] args)
     {
-        String name= "ABCDEFGHIJKLMNOPQURSTWXYZ";
-        int lang = name.length();
+        String name= "ABCDEFGHIJKLMNOPQURSTWXYZ";  // create string
+        int lang = name.length();   // use the length method 
         String rev ="";
         for(int i = lang-1;i>=0;i--)
         {
-            rev= rev + name.charAt(i);
+            rev= rev + name.charAt(i);     // use the charAt method
         } 
         System.out.println( "Reverse String "+rev);
     }
